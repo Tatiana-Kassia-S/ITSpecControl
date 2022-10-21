d59ae3667bd65d1db29408e04d4cffc298127701
Kassia <mon.kassia@gmail.com>
2022-10-21 20:12:55 +0300

    Вывод на экран заданного списка строк

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿string[] cities = {"Париж", "Рим", "Лондон", "Рио", "Берлин"}; 
+Console.Write("Дан список городов: ");
+for (int i = 0; i < cities.Length - 1; i++){
+    Console.Write($"{cities[i]}, ");
+}
+Console.WriteLine($"{cities[^1]}.");
 
